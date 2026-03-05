11e298443cbf213acd5d8baed29ec1462c92533f
yosan1921 <yosangonfa@gmail.com>
2026-03-05 12:38:56 +0300

    updated

--- backendlab/src/main/java/com/example/lms/controller/AutomatedFineController.java
+++ backendlab/src/main/java/com/example/lms/controller/AutomatedFineController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+//import java.util.Map;
 
 /**
  * AutomatedFineController - REST endpoints for automated fine processing
