074f4d56c26b903b7b6b9a402c0b9436879fdcd4
yosan1921 <yosangonfa@gmail.com>
2026-03-05 11:37:06 +0300

    updated

--- backendlab/src/main/java/com/example/lms/model/Member.java
+++ backendlab/src/main/java/com/example/lms/model/Member.java
@@ -98,7 +98,7 @@ public class Member {
      * Timestamp when the member account was created
      * Used for audit trails and membership duration tracking
      */
-    private Date ;
+    private Date createdAt;
     
     /**
      * Timestamp when the member account was last updated
