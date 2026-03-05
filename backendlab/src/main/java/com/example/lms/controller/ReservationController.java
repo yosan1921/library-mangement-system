0758cdfae43ef1e3e74854f1918e442692f82cad
yosan1921 <yosangonfa@gmail.com>
2026-03-05 12:45:57 +0300

    updated

--- backendlab/src/main/java/com/example/lms/controller/ReservationController.java
+++ backendlab/src/main/java/com/example/lms/controller/ReservationController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
-@RestController
+//@RestController
 @RequestMapping("/api/reservations")
 @CrossOrigin(origins = "*")
 public class ReservationController {
