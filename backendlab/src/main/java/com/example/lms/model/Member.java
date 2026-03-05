0aeb8a6b9ae479e5fc11eeb9d5ed3e3c64d7360f
yosan1921 <yosangonfa@gmail.com>
2026-03-05 11:34:17 +0300

    updated

--- backendlab/src/main/java/com/example/lms/model/Member.java
+++ backendlab/src/main/java/com/example/lms/model/Member.java
@@ -68,7 +68,7 @@ public class Member {
      * Contact information (phone number)
      * Used for SMS notifications and emergency contact
      */
-    private String ;
+    private String contact;
     
     /**
      * Unique membership ID assigned to each member
