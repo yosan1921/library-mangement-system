05db610d76e44481a16a718a7fdd1331268c6884
yosan1921 <yosangonfa@gmail.com>
2026-03-05 12:46:37 +0300

    updated

--- backendlab/src/main/java/com/example/lms/controller/ReservationController.java
+++ backendlab/src/main/java/com/example/lms/controller/ReservationController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 @CrossOrigin(origins = "*")
 public class ReservationController {
     
-   // @Autowired
+    @Autowired
     private ReservationService reservationService;
     
     @GetMapping
