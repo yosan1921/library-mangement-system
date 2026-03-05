d2479a574f65543d39300268e918275919b01723
yosan1921 <yosangonfa@gmail.com>
2026-03-05 12:31:18 +0300

    updated

--- backendlab/src/main/java/com/example/lms/config/MongoConfig.java
+++ backendlab/src/main/java/com/example/lms/config/MongoConfig.java
@@ -7,7 +7,7 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 // Spring framework imports for configuration and dependency injection
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 // Spring Data MongoDB configuration base class
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
