0aaff3f12656f14f2536ad2ac48e01d4828198fe
yosan1921 <yosangonfa@gmail.com>
2026-03-05 12:34:45 +0300

    updated

--- backendlab/src/main/java/com/example/lms/controller/FineController.java
+++ backendlab/src/main/java/com/example/lms/controller/FineController.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/fines")
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 public class FineController {
     
     @Autowired
